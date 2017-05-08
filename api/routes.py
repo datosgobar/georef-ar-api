91a40b78bf6ea39c07f3c35c0b166094ebd3c5a4
Arturo Peroni <arperoni@gmail.com>
2017-05-08 15:36:33 -0300

    Estructura inicial de la app Flask, con la ruta a /normalizador.

---
+++ api/routes.py
@@ -0,0 +1,10 @@
+from api import app
+from flask import jsonify
+
+
+@app.route("/api/v1.0/normalizador", methods=['GET'])
+def get_normalized_data():
+    return jsonify({
+        'estado':'SIN_RESULTADOS',
+        'direcciones': [],
+        })
