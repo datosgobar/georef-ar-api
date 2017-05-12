1a054f4f1ec6e41f2ddb8701dfa1eaffa1cbc849
Arturo Peroni <arperoni@gmail.com>
2017-05-12 11:07:44 -0300

    Tests: modifica y agrega casos de prueba.

--- tests/normalization.py
+++ tests/normalization.py
@@ -24,15 +24,22 @@ class MatchResultsTest(TestCase):
     def setUp(self):
         self.app = service.app.test_client()
 
-    def test_input_matches_single_address(self):
+    def test_get_request_with_single_match(self):
         """La dirección recibida coincide con una única dirección."""
         endpoint = '/api/v1.0/normalizador?direccion=Austria,Buenos%20Aires'
         response = self.app.get(endpoint)
         results = json.loads(response.data)
-        assert results['estado'] == 'OK' and len(results['direcciones']) == 1
+        assert len(results['direcciones']) == 1
 
-    def test_input_matches_many_addresses(self):
+    def test_get_request_with_many_matches(self):
         """La dirección recibida puede ser una de varias al normalizar."""
+        endpoint = '/api/v1.0/normalizador?direccion=Austria'
+        response = self.app.get(endpoint)
+        results = json.loads(response.data)
+        assert len(results['direcciones']) > 1
+    
+    def test_post_request_with_matches(self):
+        """La direcciones están en el cuerpo del Request, en formato JSON."""
         addresses = {'direcciones':[
             'AUSTRIA, BUENOS AIRES',
             'DIAGONAL NORTE, BUENOS AIRES']}
@@ -51,13 +58,23 @@ class RequestStatusTest(TestCase):
 
     def test_valid_request_with_results(self):
         """Request válido con resultados. Retorna OK."""
-        pass
+        endpoint = '/api/v1.0/normalizador?direccion=Austria'
+        response = self.app.get(endpoint)
+        results = json.loads(response.data)
+        assert results['estado'] == 'OK' \
+        and len(results['direcciones']) > 0
 
     def test_valid_request_with_no_results(self):
         """Request válido sin resultados. Retorna SIN_RESULTADOS."""
-        pass
+        endpoint = '/api/v1.0/normalizador?direccion=CalleQueNoExiste'
+        response = self.app.get(endpoint)
+        results = json.loads(response.data)
+        assert results['estado'] == 'SIN_RESULTADOS' \
+        and len(results['direcciones']) == 0
 
     def test_invalid_request(self):
         """Request inválido. Retorna estado INVALIDO."""
+        # This should be invalid for missing a required parameter.
         response = self.app.get('/api/v1.0/normalizador')
-        assert response.status_code == 400 and b'INVALIDO' in response.data
+        results = json.loads(response.data)
+        assert results['estado'] == 'INVALIDO' and response.status_code == 400
