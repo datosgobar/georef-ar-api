fefb5d6a174622e116397e10b1c25871f57b182b
Arturo Peroni <arperoni@gmail.com>
2017-06-13 15:46:47 -0300

    Modifica información de altura en los resultados según casos posibles. closes #26

--- service/data.py
+++ service/data.py
@@ -35,7 +35,7 @@ def search_es(address):
             {'match': {'provincia': {'query': state, 'fuzziness': 'AUTO'}}})
     result = Elasticsearch().search(body=query)
     addresses = [parse_es(hit) for hit in result['hits']['hits']]
-    if addresses and number:
+    if addresses:
         addresses = process_door(number, addresses)
     return addresses
 
@@ -87,19 +87,24 @@ def parse_osm(result):
 
 def process_door(number, addresses):
     for address in addresses:
-        info = address['observaciones']['info']
-        st_start = address.get('altura_inicial')
-        st_end = address.get('altura_final')
-        if st_start and st_end:
-            if st_start <= number and number <= st_end:
-                parts = address['nomenclatura'].split(',')
-                parts[0] += ' %s' % str(number)
-                address['nomenclatura'] = ', '.join(parts)
+        if number:
+            address['altura'] = None
+            info = address['observaciones']['info']
+            st_start = address.get('altura_inicial')
+            st_end = address.get('altura_final')
+            if st_start and st_end:
+                if st_start <= number and number <= st_end:
+                    parts = address['nomenclatura'].split(',')
+                    parts[0] += ' %s' % str(number)
+                    address['nomenclatura'] = ', '.join(parts)
+                    address['altura'] = number
+                else:
+                    info = 'La altura buscada está fuera del rango conocido.'
             else:
-                info = 'La altura buscada está fuera del rango conocido.'
-        else:
-            info = 'La calle no tiene numeración en la base de datos.'
-        address['observaciones']['info'] = info
+                info = 'La calle no tiene numeración en la base de datos.'
+            address['observaciones']['info'] = info
+        del address['altura_inicial']
+        del address['altura_final']
     return addresses
 
 
