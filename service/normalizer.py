a4516b367f9ee268de3e79ceceeb50c6da7cf80b
Arturo Peroni <arperoni@gmail.com>
2017-06-14 11:47:43 -0300

    Agrega docstrings al módulo normalizer.py.

--- service/normalizer.py
+++ service/normalizer.py
@@ -1,8 +1,24 @@
 # -*- coding: utf-8 -*-
+
+"""Módulo 'normalizer' de georef-api
+
+Contiene funciones que manejan la lógica de procesamiento
+de los recursos que expone la API.
+"""
+
 from service import data, parser
 
 
 def build_result_for(entity, matches):
+    """Arma un diccionario con la lista de resultados para una entidad.
+
+    Args:
+        entity (str): Nombre de la entidad de la que se retornan resulados.
+        matches (list): Lista con los resultados.
+
+    Returns:
+        dict: Resultados e información de estado asociada.
+    """
     return {
         'estado': 'OK' if matches else 'SIN_RESULTADOS',
         entity: matches
@@ -10,6 +26,14 @@ def build_result_for(entity, matches):
 
 
 def process(request):
+    """Procesa una consulta para normalizar direcciones.
+
+    Args:
+        request (flask.Request): Objeto con información de la consulta HTTP.
+
+    Returns:
+        Resultado de una de las funciones invocadas según el tipo de Request.
+    """
     if not parser.validate(request):
         return parser.get_response_for_invalid(request)
     if request.method == 'GET':
@@ -18,6 +42,14 @@ def process(request):
 
 
 def process_get(request):
+    """Procesa una consulta de tipo GET para normalizar direcciones.
+
+    Args:
+        request (flask.Request): Objeto con información de la consulta HTTP.
+
+    Returns:
+        Resultado de la consulta como objecto flask.Response.
+    """
     if not request.args.get('direccion'):
         return parser.get_response_for_invalid(request,
         message='El parámetro "direccion" es obligatorio.')
@@ -28,6 +60,14 @@ def process_get(request):
 
 
 def process_post(request):
+    """Procesa una consulta de tipo POST para normalizar direcciones.
+
+    Args:
+        request (flask.Request): Objeto con información de la consulta HTTP.
+
+    Returns:
+        Resultado de la consulta como objecto flask.Response.
+    """
     matches = []
     json_data = request.get_json()
     if json_data:
@@ -46,6 +86,14 @@ def process_post(request):
 
 
 def process_locality(request):
+    """Procesa una consulta de tipo GET para normalizar localidades.
+
+    Args:
+        request (flask.Request): Objeto con información de la consulta HTTP.
+
+    Returns:
+        Resultado de la consulta como objecto flask.Response.
+    """
     name = request.args.get('nombre')
     matches = data.query_entity(name, 'localidades')
     result = build_result_for('localidades', matches)
@@ -53,6 +101,14 @@ def process_locality(request):
 
 
 def process_department(request):
+    """Procesa una consulta de tipo GET para normalizar departamentos.
+
+    Args:
+        request (flask.Request): Objeto con información de la consulta HTTP.
+
+    Returns:
+        Resultado de la consulta como objecto flask.Response.
+    """
     name = request.args.get('nombre')
     matches = data.query_entity(name, 'departamentos')
     result = build_result_for('departamentos', matches)
@@ -60,6 +116,14 @@ def process_department(request):
 
 
 def process_state(request):
+    """Procesa una consulta de tipo GET para normalizar provincias.
+
+    Args:
+        request (flask.Request): Objeto con información de la consulta HTTP.
+
+    Returns:
+        Resultado de la consulta como objecto flask.Response.
+    """
     name = request.args.get('nombre')
     matches = data.query_entity(name, 'provincias')
     result = build_result_for('provincias', matches)
