0756de770aa076da951b513c3e450ff622f4becf
Arturo Peroni <arperoni@gmail.com>
2017-09-12 12:10:08 -0300

    Agrega variable POSTGRES_HOST al template de variables de entorno.

--- environment.example.sh
+++ environment.example.sh
@@ -2,6 +2,7 @@ export FLASK_APP=service/__init__.py
 export FLASK_DEBUG=0
 export OSM_API_URL='http://nominatim.openstreetmap.org/search'
 export GEOREF_URL=
+export POSTGRES_HOST=
 export POSTGRES_DBNAME=
 export POSTGRES_USER=
 export POSTGRES_PASSWORD=
