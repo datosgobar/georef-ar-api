37e75ee694446de8973b35767618637bec3dc00f
Marisol Cardozo <cardozo.marisolp@gmail.com>
2018-03-13 14:57:01 -0300

    Ordena el módulo de recursos por jerarquía de entidades

--- service/routes.py
+++ service/routes.py
@@ -10,30 +10,12 @@ from service import app, normalizer
 from flask import request
 
 
-@app.route('/api/v1.0/direcciones', methods=['GET', 'POST'])
-def get_addresses():
-    return normalizer.process_address(request)
-
-
-@app.route('/api/v1.0/calles', methods=['GET'])
-def get_streets():
-    return normalizer.process_street(request)
-
-
-@app.route('/api/v1.0/localidades', methods=['GET'])
-@app.route('/api/v1.0/localidades.csv', methods=['GET'])
-@app.route('/api/v1.0/localidades.json', methods=['GET'])
-@app.route('/api/v1.0/localidades.geojson', methods=['GET'])
-def get_localities():
-    return normalizer.process_locality(request)
-
-
-@app.route('/api/v1.0/municipios', methods=['GET'])
-@app.route('/api/v1.0/municipios.csv', methods=['GET'])
-@app.route('/api/v1.0/municipios.json', methods=['GET'])
-@app.route('/api/v1.0/municipios.geojson', methods=['GET'])
-def get_municipalities():
-    return normalizer.process_municipality(request)
+@app.route('/api/v1.0/provincias', methods=['GET'])
+@app.route('/api/v1.0/provincias.csv', methods=['GET'])
+@app.route('/api/v1.0/provincias.json', methods=['GET'])
+@app.route('/api/v1.0/provincias.geojson', methods=['GET'])
+def get_states():
+    return normalizer.process_state(request)
 
 
 @app.route('/api/v1.0/departamentos', methods=['GET'])
@@ -44,12 +26,30 @@ def get_departments():
     return normalizer.process_department(request)
 
 
-@app.route('/api/v1.0/provincias', methods=['GET'])
-@app.route('/api/v1.0/provincias.csv', methods=['GET'])
-@app.route('/api/v1.0/provincias.json', methods=['GET'])
-@app.route('/api/v1.0/provincias.geojson', methods=['GET'])
-def get_states():
-    return normalizer.process_state(request)
+@app.route('/api/v1.0/municipios', methods=['GET'])
+@app.route('/api/v1.0/municipios.csv', methods=['GET'])
+@app.route('/api/v1.0/municipios.json', methods=['GET'])
+@app.route('/api/v1.0/municipios.geojson', methods=['GET'])
+def get_municipalities():
+    return normalizer.process_municipality(request)
+
+
+@app.route('/api/v1.0/localidades', methods=['GET'])
+@app.route('/api/v1.0/localidades.csv', methods=['GET'])
+@app.route('/api/v1.0/localidades.json', methods=['GET'])
+@app.route('/api/v1.0/localidades.geojson', methods=['GET'])
+def get_localities():
+    return normalizer.process_locality(request)
+
+
+@app.route('/api/v1.0/calles', methods=['GET'])
+def get_streets():
+    return normalizer.process_street(request)
+
+
+@app.route('/api/v1.0/direcciones', methods=['GET', 'POST'])
+def get_addresses():
+    return normalizer.process_address(request)
 
 
 @app.route('/api/v1.0/ubicacion', methods=['GET'])
