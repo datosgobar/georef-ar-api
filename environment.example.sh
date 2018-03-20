f6316483126320c2dd26906e0ace0837d881188b
federicotdn <federicotedin.ar@gmail.com>
2018-03-20 16:51:02 -0300

    Remueve función save_address.

--- environment.example.sh
+++ environment.example.sh
@@ -1,7 +1,6 @@
 export FLASK_APP=service/__init__.py
 export FLASK_DEBUG=0
 export OSM_API_URL='http://nominatim.openstreetmap.org/search'
-export GEOREF_URL=
 export GEOREF_DB_HOST=
 export GEOREF_DB_NAME=
 export GEOREF_DB_USER=
