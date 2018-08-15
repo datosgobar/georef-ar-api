5f654773c7e2efb848082c3c3a4521935d38f2fe
federicotdn <federicotedin.ar@gmail.com>
2018-08-15 12:02:39 -0300

    [force_reindex] Agrega script de re-indexación con deploy.

--- bin/travis.sh
+++ bin/travis.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+# script de deploy TravisCI para georef-ar-api
 
-echo '--- Iniciando el script de deploy ---'
+readonly reindex_regex="^\[force_reindex\]"
+
+echo "- Iniciando el script de deploy para georef-ar-api..."
+
+# Lectura de variables dependiendo del entorno seleccionado
 
 case "$1" in
         production)
@@ -8,41 +13,40 @@ case "$1" in
             IP=$IP_PROD
             PORT_SSH=$PORT_SSH_PROD
             USER_SSH=$USER_SSH_PROD
-            PASS_SSH=$PASS_SSH_PROD
             BRANCH=$BRANCH_PROD
             ;;
-         
+
         staging)
             echo '- Entorno: staging'
             IP=$IP_STG
             PORT_SSH=$PORT_SSH_STG
             USER_SSH=$USER_SSH_STG
-            PASS_SSH=$PASS_SSH_STG
             BRANCH=$BRANCH_STG
             ;;
-         
+
         development)
             echo '- Entorno: development'
             IP=$IP_DEV
             PORT_SSH=$PORT_SSH_DEV
             USER_SSH=$USER_SSH_DEV
-            PASS_SSH=$PASS_SSH_DEV
             BRANCH=$BRANCH_DEV
-            ;;         
+            ;;
         *)
             echo 'No se especificó un entorno.'
             exit 1
 esac
 
-echo '- Agregando host a known hosts...'
-ssh-keyscan -p $PORT_SSH $IP >> ~/.ssh/known_hosts
+echo "- Agregando host a known hosts..."
+ssh-keyscan -p "$PORT_SSH" "$IP" >> ~/.ssh/known_hosts
+
+# Re-deploy de aplicación Flask y reinicio de servicio
 
 DEPLOY_SCRIPT="
-cd ${GEOREF_API_DIR};
+cd $GEOREF_API_DIR;
 echo '- Activando virtualenv...'
-. venv/bin/activate
+source venv/bin/activate
 echo '- Pulleando branch git...'
-git pull origin ${BRANCH}
+git pull origin $BRANCH
 echo '- Actualizando dependencias...'
 pip install --upgrade --force-reinstall -r requirements.txt
 echo '- Reiniciando servicio...'
@@ -50,5 +54,20 @@ sudo systemctl restart georef-api
 echo 'Listo.'
 "
 
-echo '- Haciendo ssh a la máquina y corriendo script de deploy'
-ssh -p $PORT_SSH $USER_SSH@$IP "${DEPLOY_SCRIPT}"
+echo "- Haciendo ssh a la máquina y corriendo script de deploy..."
+ssh -p "$PORT_SSH" "$USER_SSH"@"$IP" "$DEPLOY_SCRIPT"
+
+# Re-indexación opcional de datos
+
+REINDEX_SCRIPT="
+cd $GEOREF_API_DIR
+source venv/bin/activate
+make index_forced
+"
+
+if [[ "$TRAVIS_COMMIT_MESSAGE" =~ $reindex_regex ]]; then
+    echo "- Activando script de re-indexación..."
+    ssh -p "$PORT_SSH" "$USER_SSH"@"$IP" "$REINDEX_SCRIPT"
+else
+    echo "- No se especificó una re-indexación."
+fi
